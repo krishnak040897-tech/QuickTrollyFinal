56679808a6a3792d16b92913d8b73aae14dcd795
krishnak040897-tech <krishnak040897@gmail.com>
2026-09-17 22:58:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 import os
 
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'quicktrolly-secret-key-2026'
     DATABASE_URL = os.environ.get('DATABASE_URL') or 'postgresql://user:password@localhost:5432/quicktrolly_db'
     RAZORPAY_KEY_ID = os.environ.get('RAZORPAY_KEY_ID') or 'rzp_test_dhYJFlohg88eyl'
     RAZORPAY_KEY_SECRET = os.environ.get('RAZORPAY_KEY_SECRET') or 'YOUR_SECRET_KEY_HERE'
-    
+
     # Cloudinary Configuration
     CLOUDINARY_CLOUD_NAME = os.environ.get('CLOUDINARY_CLOUD_NAME')
     CLOUDINARY_API_KEY = os.environ.get('CLOUDINARY_API_KEY')
